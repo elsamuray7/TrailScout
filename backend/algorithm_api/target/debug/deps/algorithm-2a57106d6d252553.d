27fcc9387399b262f9b910db52b1f02bb69e6b23
elsamuray7 <samuel.dorian@outlook.de>
2022-05-31 14:56:01 +0200

    applied gitignore

--- backend/algorithm_api/target/debug/deps/algorithm-2a57106d6d252553.d
+++
@@ -1,7 +0,0 @@
-C:\Users\samue\OneDrive\Desktop\Studium\MoSc_SWT\Sem3\EnPro_FMI\EnProFMI2022\algorithm\target\debug\deps\algorithm-2a57106d6d252553.exe: src\lib.rs src\algorithm\mod.rs src\algorithm\greedy.rs
-
-C:\Users\samue\OneDrive\Desktop\Studium\MoSc_SWT\Sem3\EnPro_FMI\EnProFMI2022\algorithm\target\debug\deps\algorithm-2a57106d6d252553.d: src\lib.rs src\algorithm\mod.rs src\algorithm\greedy.rs
-
-src\lib.rs:
-src\algorithm\mod.rs:
-src\algorithm\greedy.rs:
