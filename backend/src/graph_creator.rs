d62855363b6c94a87392b0145ec2699e7061229d
Leonard Rupietta <st149207@stud.uni-stuttgart.de>
2022-08-01 15:14:40 +0200

    Refactor in graph_creator.rs and osm_graph_creator.rs - Now 1 function to build fmi graph_creator.rs To build graph standalone now requires commandline args as explained in readme and wiki

--- backend/src/graph_creator.rs
+++ backend/src/graph_creator.rs
@@ -1,37 +1,30 @@
 use std::io;
 use std::time::Instant;
 use env_logger::Env;
+use std::env;
 use trailscout_lib::data::graph::{Edge, Sight, Node, Graph};
-use trailscout_lib::data::osm_graph_creator::{parse_osm_data, write_graph_file};
+use trailscout_lib::data::osm_graph_creator::{create_fmi_graph, parse_osm_data, write_graph_file};
+use clap::Parser;
 #[macro_use]
 extern crate log;
 
-fn main() -> Result<(), io::Error> {
+pub fn main() -> Result<(), io::Error> {
     //initializing the logger
-    let env = Env::default()
+    let env_logger = Env::default()
     .filter_or("TRAILSCOUT_LOG_LEVEL", "trace")
     .write_style_or("TRAILSCOUT_LOG_STYLE", "always");
-    env_logger::init_from_env(env);
+    env_logger::init_from_env(env_logger);
     info!("starting up");
 
-    let in_graph = "./osm_graphs/germany-latest.osm.pbf";
-    let out_graph = "./osm_graphs/germany-latest.fmi";
-    let mut nodes : Vec<Node> = Vec::new();
-    let mut edges : Vec<Edge> = Vec::new();
-    let mut sights : Vec<Sight> = Vec::new();
-    parse_osm_data(in_graph, &mut nodes, &mut edges, &mut sights);
-    write_graph_file( out_graph, &mut nodes, &mut edges, &mut sights);
+    let args: Vec<String> = env::args().collect();
 
-    info!("Start creating the graph from fmi file!");
-    let time_start = Instant::now();
+    println!("Input file is {}.", &args[1]);
+    println!("Output file is {}.", &args[2]);
 
-    let graph = Graph::parse_from_file(out_graph).unwrap();
+    //let in_graph = "./osm_graphs/bremen-latest.osm.pbf";
+    //let out_graph = "./osm_graphs/bremen-latest.fmi";
 
-    let time_duration = time_start.elapsed();
-    info!("End graph creation after {} seconds!", time_duration.as_secs());
-
-    println!("{}", graph.num_nodes);
-    println!("{}", graph.num_sights);
-    println!("{}", graph.num_edges);
-    Ok(())
+    let in_graph = &args[1];
+    let out_graph = &args[2];
+    create_fmi_graph(in_graph,out_graph)
 }
