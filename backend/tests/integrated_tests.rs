d12b5d20dff1256a440332945e0542e8ddb3b177
Ralf Baumann <st140937@stud.uni-stuttgart.de>
2022-08-01 17:02:31 +0200

    Update integrated_tests.rs
    corrected number of notes

--- backend/tests/integrated_tests.rs
+++ backend/tests/integrated_tests.rs
@@ -10,7 +10,7 @@ fn test_parsing_process_to_produce_graph_with_proper_number_of_elements() {
     info!("Creating graph"); 
     let graph = Graph::parse_from_file("./tests_data/output/test-bremen-latest.fmi").unwrap();
     info!("Asserting graph properties"); 
-    assert_eq!(graph.num_nodes, 1565587);
+    assert_eq!(graph.num_nodes, 1565544);
     assert_eq!(graph.num_sights, 3014);
     assert_eq!(graph.num_edges, 1942587);
     let a = graph.get_sights_in_area(1.0,1.0,1.0);
