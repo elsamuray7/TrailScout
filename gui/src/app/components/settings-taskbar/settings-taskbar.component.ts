f35192e250e5377680a76801272f220a6027f847
Dominic Feldweg <st149630@stud.uni-stuttgart.de>
2022-08-10 19:31:22 +0200

    Convert route request radius to m

--- gui/src/app/components/settings-taskbar/settings-taskbar.component.ts
+++ gui/src/app/components/settings-taskbar/settings-taskbar.component.ts
@@ -106,7 +106,7 @@ export class SettingsTaskbarComponent implements OnInit {
       "area": {
         "lat": this.mapService.getCoordniates().lat,
         "lon": this.mapService.getCoordniates().lng,
-        "radius": this.mapService.getRadius()
+        "radius": this.mapService.getRadius() * 1000
       },
       "user_prefs": {
         "categories": categories,
